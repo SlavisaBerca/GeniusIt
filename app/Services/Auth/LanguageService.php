52a113ea0a8562c422c0a5fb30f3e1cc40cb81e8
Slavisa Berca <slavisaberca69@gmail.com>
2024-02-13 15:47:39 +0200

    services_for_all

---
+++ app/Services/Auth/LanguageService.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Services\Auth;
+
+class LanguageService
+{
+    public static function getLang()
+    {
+
+    }
+    public static function setLang()
+    {
+
+    }
+
+    public static function setContent()
+    {
+        
+    }
+}
