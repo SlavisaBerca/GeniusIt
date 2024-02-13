96177de5ebbdd150826e4655bcad55869e4d1629
Slavisa Berca <slavisaberca69@gmail.com>
2024-02-13 15:46:09 +0200

    set_services

---
+++ app/Services/Interface/LanguageService.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Services\Interface;
+
+class LanguageService
+{
+    public static function getLang()
+    {
+
+    }
+    public static function setLang()
+    {
+
+    }
+
+    public static function setContent()
+    {
+        
+    }
+}
